e416c2f7d5ba875d8ab8ad11bd9218df699c127c
mktcode <kontakt@markus-kottlaender.de>
2021-06-01 08:05:52 +0200

    trying to fix timeouts

--- truffle-config.js
+++ truffle-config.js
@@ -66,6 +66,7 @@ module.exports = {
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      networkCheckTimeout: 1000000000, // a desperate attempt
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
     // Useful for private networks
